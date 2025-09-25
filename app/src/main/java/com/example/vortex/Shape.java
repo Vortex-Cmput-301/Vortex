1665c47df120274457675f3efff00d3ca539b1a6
asehmb <130265153+asehmb@users.noreply.github.com>
2025-09-24 18:32:50 -0600

    Update Shape.java

--- app/src/main/java/com/example/vortex/Shape.java
+++ app/src/main/java/com/example/vortex/Shape.java
@@ -3,7 +3,7 @@ package com.example.vortex;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "purple";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
 
 
