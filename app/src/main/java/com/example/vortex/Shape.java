2522bc49afc139963dd6073f37830a7905e28e79
Benjamin Nguyen <BenNguyenv182@gmail.com>
2025-09-24 18:39:11 -0600

    added main color

--- app/src/main/java/com/example/vortex/Shape.java
+++ app/src/main/java/com/example/vortex/Shape.java
@@ -5,8 +5,6 @@ public abstract class Shape {
     private int y;
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
-    String color = "Black";
-
     Shape(int x, int y){
         this.x = x;
         this.y = y;
