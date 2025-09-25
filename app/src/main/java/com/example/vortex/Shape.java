38707f92d8610ac3748ab3e2a497f20806db9ff2
Salty876 <salabdoulaye876@gmail.com>
2025-09-24 18:18:45 -0600

    did my color ered

--- app/src/main/java/com/example/vortex/Shape.java
+++ app/src/main/java/com/example/vortex/Shape.java
@@ -3,10 +3,12 @@ package com.example.vortex;
 public abstract class Shape {
     private int x;
     private int y;
-
+    private String color;
     Shape(int x, int y){
         this.x = x;
         this.y = y;
+        this.color = "Volcano red"
+
 ;    }
 
 //    new branch
